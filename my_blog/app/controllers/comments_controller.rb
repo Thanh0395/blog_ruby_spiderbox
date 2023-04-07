1ab73cd3beb7e56ae373291771d4fec7a30d1cdf
Tran Pham Duy Thanh <thanhmegaman23395@gmail.com>
2023-04-07 11:10:13 +0700

    them dc comment

--- my_blog/app/controllers/comments_controller.rb
+++ my_blog/app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(params[:comment])
+        @comment = Comment.create(:commenter => params[:comment][:commenter], :body => params[:comment][:body], :post_id => params[:post_id])
         redirect_to post_path(@post)
     end
 
@@ -12,4 +12,8 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post)
     end
     
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body, :post_id)
+        end
 end
